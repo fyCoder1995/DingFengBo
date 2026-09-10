76a337fca34964b42f771c9528e569ad9e5b3187
fycoder <gemma.mobu@icloud.com>
2026-09-10 16:04:01 +0800

    Site updated: 2026-09-10 16:03:46

--- HTML/3D_Granary/camera.js
+++ HTML/3D_Granary/camera.js
@@ -1,12 +1,7 @@
 import * as THREE from 'three';
 
-var k = window.innerWidth / window.innerHeight; 
-var s = 100;
-// var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 3000);
-
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 3000);
-
-camera.position.set(320,160,200);
+var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(100, 100, 100); 
 camera.lookAt(0, 0, 0); 
 
 export default camera;
--- HTML/3D_Granary/renderer.js
+++ HTML/3D_Granary/renderer.js
@@ -1,8 +1,7 @@
 import * as THREE from "three";
+import mesh from "./scene/mesh.js";
 import scene from "./scene/index.js";
 import camera from "./camera.js";
-import { labelRenderer } from "./scene/tag.js";
-import { massageBoxRenderer } from "./scene/messageTag.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 var renderer = new THREE.WebGLRenderer({
@@ -10,19 +9,20 @@ var renderer = new THREE.WebGLRenderer({
 });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.outputColorSpace = THREE.SRGBColorSpace;
-// renderer.toneMapping = THREE.ACESFilmicToneMapping;  // 电影级，默认
-// renderer.toneMappingExposure = 1.2;  // 可调 0.8~1.2
-renderer.setClearColor(0x005577, 1);
+
+// renderer.render(scene, camera);
 var controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
+// controls.update();
+// controls.addEventListener("change", () => {
+//   renderer.render(scene, camera);
+// });
 
 function render() {
+  mesh.rotateX(0.01);
   renderer.render(scene, camera);
-  labelRenderer.render(scene, camera);
-  massageBoxRenderer.render(scene, camera);
   requestAnimationFrame(render);
 }
 
 render();
+
 export default renderer;
